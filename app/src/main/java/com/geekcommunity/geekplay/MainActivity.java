2393089d8fc9e3fe6aa33d2771835230333cf61a
Mohamed Fayas Khan <mohamedfayaskhan.a@gmail.com>
2021-12-18 13:03:01 +0530

    Version 1.0

--- app/src/main/java/com/geekcommunity/geekplay/MainActivity.java
+++ app/src/main/java/com/geekcommunity/geekplay/MainActivity.java
@@ -2,7 +2,11 @@ package com.geekcommunity.geekplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        splashShow();
+    }
+
+    private void splashShow() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        int TIME = 2000;
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), ListOfVideos.class));finish();
+        }, TIME);
     }
 }
